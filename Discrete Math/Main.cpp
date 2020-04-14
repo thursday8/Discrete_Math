87fb427d28102db56a040ed9fb47fb0c2815eb46
Danil Lamov <Danil_lamov@mail.ru>
2020-04-14 18:48:41 +0300

    Add files via upload
    Added basic project files

---
+++ Discrete Math/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <conio.h>
+#include "Natural numbers with zero.h"
+using namespace std;
+
+int main() {
+	cout << "Discrete Math" << endl;
+	cout << SUB_NDN_N(18, 18, 1) << endl;
+	return 0;
+}
---
+++ Discrete Math/Natural numbers with zero.h
@@ -0,0 +1,21 @@
+#ifndef NATURAL_NUMBERS_WITH_ZERO_H
+#define NATURAL_NUMBERS_WITH_ZERO_H
+
+//Юлия Веселкова
+const unsigned long int ADD_1N_N(const unsigned long int& n);
+const long int MUL_ND_N(const unsigned long int& n, const long int& m);
+const long int SUB_NDN_N(const unsigned long int& n, const unsigned long int& m, const long int& k);
+const unsigned long int  MOD_NN_N(const unsigned long int& n, const unsigned long int& m);
+const unsigned long int  LCM_NN_N(const unsigned long int& n, const unsigned long int& m);
+
+//Валерия Веревина
+const int COM_NN_D(const unsigned long int& n, const unsigned long int& m);
+const unsigned long int ADD_NN_N(const unsigned long int& n, const unsigned long int& m);
+const unsigned long int MUL_Nk_N(const unsigned long int& n, const unsigned int& k);
+
+//Анастасия Данченкова
+void NZER_N_B(const unsigned long int& n);
+const unsigned long int SUB_NN_N(const unsigned long int& n, const unsigned long int& m);
+const unsigned long int MUL_NN_N(unsigned long int n, unsigned long int m);
+
+#endif
