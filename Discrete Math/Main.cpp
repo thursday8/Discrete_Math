41fb16327460e82aa3d28f40514780bac7b7f8df
Danil Lamov <Danil_lamov@mail.ru>
2020-04-17 16:01:43 +0300

    Add files via upload
    Update

--- Discrete Math/Main.cpp
+++ Discrete Math/Main.cpp
@@ -7,6 +7,8 @@ int main() {
 	setlocale(LC_ALL, "ru");
 	cout << "Discrete Math" << endl;
 	NN* a = readNN();
+	printNN(a);
+	printNN(MUL_Nk_N(a, 20));
 
 	return 0;
 }
--- Discrete Math/NN.h
+++ Discrete Math/NN.h
@@ -6,15 +6,19 @@ struct NN;
 //Данил Ламов
 NN* readNN();
 void printNN(NN* n);
+NN* delNN(NN* n);
 
 //Валерия Веревина
 int COM_NN_D(NN* n, NN* m);
+NN* ADD_NN_N(NN* n, NN* m);
+NN* MUL_Nk_N(NN* n, const unsigned int& k);
 
 //Анастасия Данченкова
 void NZER_N_B(NN* n);
+NN* SUB_NN_N(NN* n, NN* m);
 
 //Юлия Веселкова
 NN* ADD_1N_N(NN* n);
-
+NN* MUL_ND_N(NN* n, const char& num);
 
 #endif
